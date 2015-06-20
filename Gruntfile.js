11bbafa37a4522ef00447de28642c077e99b2399
zhou-yg <zhou.y.g890@gmail.com>
2015-06-20 23:51:15 +0800

    episodes interface is complete

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,8 @@
-module.exports = function(grunt){
+module.exports = function (grunt) {
     grunt.initConfig({
-        clean:{
-            files:['public/js/**','public/styles/**']
+        clean: {
+            cssFiles: [ 'public/styles/**'],
+            jsFiles:['public/js/**']
         },
         coffee: {
             files: {
@@ -12,19 +13,32 @@ module.exports = function(grunt){
                 ext: '.js'
             }
         },
-        less:{
-            files:{
-                expand:true,
-                cwd:'precompile/less/',
-                src: ['**/*.less'],
+        less: {
+            files: {
+                expand: true,
+                cwd: 'precompile/less/',
+                //带下划线的都是分支，会自动import到总的中，不用额外编译了
+                src: ['**/*.less','!**/*_*.less'],
                 dest: 'public/styles/',
                 ext: '.css'
             }
+        },
+        watch: {
+            options: {
+                dateFormat: function(time) {
+                    grunt.log.writeln('The watch finished in ' + time + 'ms at' + (new Date()).toString());
+                    grunt.log.writeln('Waiting for more changes...');
+                }
+            },
+            lessTask:{
+                files:['**/*.less'],
+                tasks:['clean:cssFiles','less']
+            }
         }
-
     });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-coffee');
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.registerTask('default', ['clean','coffee','less']);
+    grunt.registerTask('default', ['watch']);
 };
