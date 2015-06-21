3d787fa379fcd14c77acd5a808e816a089735f15
zhou-yg <zhou.y.g890@gmail.com>
2015-06-21 22:15:05 +0800

    v0.1 UI is ready

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+// app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //---------------------------------------------------
 var routesDirPath = './routes/';
-var routes = require(routesDirPath+'index');
+var routes = require(routesDirPath+'index/main');
 
 app.use('/', routes);
 
